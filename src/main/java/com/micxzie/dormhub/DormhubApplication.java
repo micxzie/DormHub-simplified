ef26acca231b678a2b8dc1269019db69ad209851
micxzie <micxzie850@gmail.com>
2026-07-07 18:40:57 +0800

    init: spring boot scaffold with mysql connection and permissive security config

---
+++ src/main/java/com/micxzie/dormhub/DormhubApplication.java
@@ -0,0 +1,13 @@
+package com.micxzie.dormhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micxzie/dormhub/DormhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micxzie.dormhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
